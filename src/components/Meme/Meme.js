493cf5c03423b023289af46c9fe4338952c03ab6
mutuotakelvin <kelvinmutuota749@gmail.com>
2022-01-31 02:30:06 +0300

    Fetch data externally

--- src/components/Meme/Meme.js
+++ src/components/Meme/Meme.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import './Meme.scss'
-import memesData from './../../MemeData/MemesData'
 
 const Meme = () => {
     const [meme, setMeme] = useState({
@@ -8,17 +7,18 @@ const Meme = () => {
         bottomText: "",
         randomImage: "http://i.imgflip.com/1bij.jpg" 
     })
-    const [allMemeImages, setAllMemeImages] = React.useState(memesData)
+    const [allMemes, setAllMemes] = React.useState([])
     
-    
+
     function getMemeImage() {
-        const memesArray = allMemeImages.data.memes
+        const memesArray = allMemes
         const randomNumber = Math.floor(Math.random() * memesArray.length)
         const url = memesArray[randomNumber].url
         setMeme(prevMeme => ({
             ...prevMeme,
             randomImage: url
         }))
+        
     }
 
     const handleChange = (e)=>{
@@ -28,6 +28,13 @@ const Meme = () => {
             [name]:value
         }))
     }
+
+    React.useEffect(()=>{
+        fetch("https://api.imgflip.com/get_memes")
+        .then(res=>res.json())
+        .then(data => setAllMemes(data.data.memes))
+    },[])
+
     return (
         <main>
             <div className='form'>
